d8d93e09ca722178f33933bf673b85d6794d32e4
SeanDMR <sean@flyinggrizzly.io>
2017-05-20 19:01:06 +0100

    add test for index view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,4 +13,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(user.name)
     assert_match "Role: <strong>Admin</strong>", response.body
   end
+
+  test 'should index users' do
+    get users_path
+    assert_response :success
+    users.each do |user|
+      assert_select "li#user-#{user.id}"
+      assert_select 'h3', user.name
+      assert_select 'h5', "Role: #{user_role(user)}"
+    end
+  end
 end
