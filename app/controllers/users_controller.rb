eea0cee8648f0dec58fcdcb6240ba41790da5679
SeanDMR <sean@flyinggrizzly.io>
2017-05-20 16:32:33 +0100

    add error messages partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # handle success
     else
@@ -14,5 +14,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email,
+                                 :password,
+                                 :password_confirmation)
   end
 end
