fa191e0ccf04c5d0a44922f12053b3329978d815
SeanDMR <sean@flyinggrizzly.io>
2017-05-20 23:23:55 +0100

    add remembered sessions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -56,4 +56,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', users_path,    count: 0
     # assert_select 'a[href=?]', aliases_path,  count: 0
   end
+
+  test 'memorious login' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test 'forgetful login' do
+    # Log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+
+    # Log in again and test that the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
