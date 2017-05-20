fdf0119b86a205e73f3122765b3545d4b4484939
SeanDMR <sean@flyinggrizzly.io>
2017-05-20 15:12:22 +0100

    add user show action

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,3 @@
 module StaticPagesHelper
-  def full_title(page_title = '')
-    base_title = 'url shortener beta'
-    if page_title.empty?
-      base_title
-    else
-      base_title + ' | ' + page_title
-    end
-  end
+  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_path
     assert_response :success
   end
 
+  test 'should show user' do
+    user = users(:cthulhu)
+    get user_path(user)
+    assert_response :success
+    assert_select 'title', full_title(user.name)
+    assert_match "Role: <strong>#{user.role}</strong>", response.body
+  end
 end
