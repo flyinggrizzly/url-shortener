091346fc14d69dde80749f012db01379706aa5e3
SeanDMR <sean@flyinggrizzly.io>
2017-05-27 15:39:40 +0100

    add new, show views and controller methods for short)urls

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,2 +1,16 @@
 class ShortUrlsController < ApplicationController
+  def index
+    @short_urls = ShortUrl.all
+  end
+
+  def show
+    @short_url = ShortUrl.find(params[:id])
+  end
+
+  def new
+    @short_url = ShortUrl.new
+  end
+
+  def create
+  end
 end
--- test/controllers/short_urls_controller_test.rb
+++ test/controllers/short_urls_controller_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class ShortUrlsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = users(:cthulhu)
+
+    @short_url = short_urls(:example)
+  end
+
+  test 'admins can get index' do
+    log_in_as(@admin)
+    get short_urls_path
+    assert_response :success
+  end
+
+  test 'admins can get show' do
+    log_in_as(@admin)
+    get short_url_path(@short_url)
+    assert_response :success
+  end
+
+  test 'admins can get new' do
+    log_in_as(@admin)
+    get new_short_url_path
+    assert_response :success
+  end
+
+  test 'admins can post create' do
+  end
+
+  test 'admins can get edit' do
+  end
+
+  test 'admins can patch update' do
+  end
+
+  test 'admins can get delete' do
+  end
+
+  test 'admins can destroy' do
+  end
+
 end
