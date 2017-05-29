a0078aa2b304f399b1ecfdf8a37c41f9e04c448e
SeanDMR <sean@flyinggrizzly.io>
2017-05-29 15:46:04 +0100

    fix seedfile syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,89 @@
-User.create!(name:                  <%= ENV['ADMIN_NAME'] %>,
-             email:                 <%= ENV['ADMIN_EMAIL'] %>,
+User.create!(name:                  ENV['ADMIN_NAME'],
+             email:                 ENV['ADMIN_EMAIL'],
              admin:                 true,
-             password:              <%= ENV['ADMIN_PASSWD'] %>,
-             password_confirmation: <%= ENV['ADMIN_PASSWRD'] %>)
+             password:              ENV['ADMIN_PASSWD'],
+             password_confirmation: ENV['ADMIN_PASSWRD'])
 
 ShortUrl.create!(slug: '2osPc0Y', redirect: 'http://flyinggrizzly.io/2017/04/11/test-should-bring-joy-do/')
+
 ShortUrl.create!(slug: '2nuwLJN', redirect: 'http://flyinggrizzly.io/2017/03/17/ruby-noob-bundler-rake-guard/')
-ShortUrl.create!(slug: '2mILD66', redirect: 'http://flyinggrizzly.io/2017/03/10/open-letter-to-joss-whedon-thank-you/')\n
+
+ShortUrl.create!(slug: '2mILD66', redirect: 'http://flyinggrizzly.io/2017/03/10/open-letter-to-joss-whedon-thank-you/')
+
 ShortUrl.create!(slug: '2lY5z5p', redirect: 'http://www.artofmanliness.com/2012/11/20/power-of-habits/')
+
 ShortUrl.create!(slug: '2k7f6m1', redirect: 'http://flyinggrizzly.io/2017/02/14/writing-my-first-gem/')
+
 ShortUrl.create!(slug: '2juNSVD', redirect: 'https://www.theguardian.com/us-news/2017/jan/30/anti-trump-petition-to-prevent-uk-state-visit-passes-1m-signatures')
+
 ShortUrl.create!(slug: '2jjBX1B', redirect: 'https://petition.parliament.uk/petitions/171928')
+
 ShortUrl.create!(slug: '2keAfxE', redirect: 'http://flyinggrizzly.io/2017/01/29/setting-up-borg/')
+
 ShortUrl.create!(slug: '2kagsAp', redirect: 'http://flyinggrizzly.io/2017/01/20/quest-reach-philosphy-on-wikipedia/')
+
 ShortUrl.create!(slug: '2jl1WEB', redirect: 'http://flyinggrizzly.io/2017/01/15/lunanode-review/')
+
 ShortUrl.create!(slug: '2j7w5oI', redirect: 'http://flyinggrizzly.io/2017/01/13/worldbuilding-for-dnd-like-a-dev-wannabe')
+
 ShortUrl.create!(slug: '2jbAcij', redirect: 'http://flyinggrizzly.io/2017/01/06/eggs-half-a-dict/')
+
 ShortUrl.create!(slug: '2iLta4F', redirect: 'http://flyinggrizzly.io/2017/01/02/learning-by-doing-what-you-want/')
+
 ShortUrl.create!(slug: '2eC5xbL', redirect: 'http://arstechnica.com/gaming/2016/10/communitys-donald-glover-is-the-next-young-lando-calrissian/')
+
 ShortUrl.create!(slug: '2exTmg0', redirect: 'https://youtu.be/HBBwXAPNLr0')
+
 ShortUrl.create!(slug: '2dZBZJK', redirect: 'https://www.macstories.net/reviews/tableflip-takes-the-pain-out-of-multimarkdown-tables/')
+
 ShortUrl.create!(slug: '2e1J7Vi', redirect: 'http://www.ilxportal.com/sites/bestpractice2/startproduct.asp?first=false&pac=6db982c3447&')
+
 ShortUrl.create!(slug: '2dFzZ9s', redirect: 'http://www.alfredforum.com/topic/935-workflowshorten-url-support-googl-bitly-tcn-jmp-isgd-vgd/')
+
 ShortUrl.create!(slug: '2dxfirh', redirect: 'https://www.amazon.co.uk/dp/B00G0N8M2Y/ref=dv_web_wtls_list_pr_4')
+
 ShortUrl.create!(slug: '2dhv9fO', redirect: 'https://www.google.co.uk/search?q=goo.gl+vs+bit.ly&oq=goo.gl+vs+bit.ly&aqs=chrome..69i57.4957j0j7&sourceid=chrome&ie=UTF-8#q=goo.gl+vs+bit.ly&safe=off&start=10')
+
 ShortUrl.create!(slug: '2dhdXHb', redirect: 'http://www.imdb.com/title/tt0269217/')
+
 ShortUrl.create!(slug: '2cTyxxL', redirect: 'https://boardgamegeek.com/boardgame/15987/arkham-horror')
+
 ShortUrl.create!(slug: '1NvcYkA', redirect: 'http://flyinggrizzly.io/blog/2015/4/12/app-review-bartender')
+
 ShortUrl.create!(slug: '1alkdck', redirect: 'http://flyinggrizzly.io/blog/2015/4/12/the-mouse-is-your-enemy')
+
 ShortUrl.create!(slug: '1Cxt1CJ', redirect: 'http://0.0.0.2/')
+
 ShortUrl.create!(slug: '1EockAD', redirect: 'http://stackoverflow.com/questions/160930/how-do-i-check-if-an-integer-is-even-or-odd')
+
 ShortUrl.create!(slug: '19Jhzhd', redirect: 'https://www.youtube.com/watch?v=-edi_WqjKro')
+
 ShortUrl.create!(slug: '19JfMst', redirect: 'https://www.reddit.com/r/cs50/')
+
 ShortUrl.create!(slug: '19Jfi5Q', redirect: 'http://daringfireball.net/linked/2015/03/02/vesper-price')
+
 ShortUrl.create!(slug: '18Rf63r', redirect: 'https://gist.github.com/flyinggrizzly/68f554c14cab4802389c')
+
 ShortUrl.create!(slug: '1DKPD9o', redirect: 'http://flyinggrizzly.io/blog/2015/3/22/app-review-boxcryptor')
+
 ShortUrl.create!(slug: '1GDDP6k', redirect: 'http://flyinggrizzly.io/blog/techcrunch-battle-for-the-customer-interface/2015/3/21')
+
 ShortUrl.create!(slug: '1xog4jc', redirect: 'http://techcrunch.com/2015/03/03/in-the-age-of-disintermediation-the-battle-is-all-for-the-customer-interface/')
+
 ShortUrl.create!(slug: '1EzA9jr', redirect: 'http://flyinggrizzly.io/blog/app-showdown-use-iphone-to-unlock-your-mac/2015/3/21')
+
 ShortUrl.create!(slug: '1Fxz2os', redirect: 'http://t.co/jKMWujhl23')
+
 ShortUrl.create!(slug: '1xc5mfq', redirect: 'https://vimeo.com/122368314')
+
 ShortUrl.create!(slug: '1EhyAXh', redirect: 'http://pic.twitter.com/tc1CX0J8Dd')
+
 ShortUrl.create!(slug: '1Ehy8II', redirect: 'http://flyinggrizzly.io/')
+
 ShortUrl.create!(slug: '1Ehy5MR', redirect: 'http://flyinggrizzly.io/blogfrontpage/')
+
 ShortUrl.create!(slug: '1HUkLlv', redirect: 'https://workflow.is/developer')
+
 ShortUrl.create!(slug: '19nPnAx', redirect: 'http://flyinggrizzly.io/blog/app-review-mouse-warp/2015/3/10')
+
 ShortUrl.create!(slug: '1GHrNZp', redirect: 'http://flyinggrizzly.io/blog/in-memoriam-magsafe/2015/3/15')
