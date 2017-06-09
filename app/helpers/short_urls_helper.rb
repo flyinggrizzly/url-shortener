25d8593e40299f109f4d4c14a72c8000efea8919
SeanDMR <sean@flyinggrizzly.io>
2017-06-09 20:21:36 +0100

    have working random slug code

--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,9 +1,2 @@
 module ShortUrlsHelper
-  require 'radix'
-
-  # Slug generator alphabet
-  def base_37_convert(number)
-    base_37_alphabet = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z -)
-    number.b(10).to_s(base_37_alphabet)
-  end
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,6 +1,6 @@
 class ShortUrl < ApplicationRecord
   require 'uri'
-  include ShortUrlsHelper
+  require 'radix'
 
   ### ATTRIBUTES ###############################
   # UrlAlias model has the following attributes:
@@ -46,7 +46,38 @@ class ShortUrl < ApplicationRecord
 
     # Generates a random slug
     def random_slug(length = UrlGrey::Application.config.random_slug_length)
+      # If the app has a random current slug counter, use that
+      if slug_id = AppConfig.current_random_slug
+        # Generate the slug
+        slug = base_37_convert(slug_id)
+
+        # Pad out shorter slugs to meet length param
+        while slug.length < length
+          slug.insert(0, '0')
+        end
+
+        # Make sure slug does not exist and doesn't include hyphens, or return it
+        if slug.include?('-')
+          AppConfig.current_random_slug += 1
+          random_slug(length)
+        elsif ShortUrl.find_by(slug: slug)
+          AppConfig.current_random_slug += 1
+          random_slug(length)
+        else
+          return slug
+        end
       
+      # Otherwise create the counter and recall the method
+      else
+        AppConfig.current_random_slug = 0
+        random_slug(length)
+      end
+    end
+
+    # Slug generator alphabet using the Radix gem
+    def base_37_convert(number)
+      base_37_alphabet = %w[0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z -]
+      number.b(10).to_s(base_37_alphabet)
     end
   end
 
@@ -65,4 +96,5 @@ class ShortUrl < ApplicationRecord
   def ensure_scheme
     self.redirect.insert(0, 'http://') unless URI.parse(self.redirect).scheme
   end
+
 end
