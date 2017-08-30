87c69566675720b4d7051cfcc625cd9b3582a116
SeanDMR <sean@flyinggrizzly.io>
2017-08-30 15:59:27 +0100

    finish feature spec scenario with good batch short URLs

--- spec/features/short_urls_spec.rb
+++ spec/features/short_urls_spec.rb
@@ -18,7 +18,7 @@ RSpec.feature "ShortUrls", type: :feature do
   #   end
   # end
 
-  scenario 'user creates short URLs in a batch', aggregate_failures: true, batch_test: true do
+  scenario 'user submits a valid batch of short URLs', aggregate_failures: true, batch_test: true do
     user = FactoryGirl.create(:admin_user) # batch operations are restricted to admins
 
     visit root_or_admin_path
@@ -32,16 +32,21 @@ RSpec.feature "ShortUrls", type: :feature do
     attach_file  'short_urls_csv', 'spec/misc/batch.csv'
     click_button 'Create short URLs'
 
+    # track slugs being updated and created to test success in a minute
+    updated_slugs = []; created_slugs = []
+
     # Expect each of the entries in the CSV to be present in the form...
     CSV.read('spec/misc/batch.csv').each do |slug, redirect|
       # ... those that need to be updated
-      if slug.include? 'pre-exists'  
+      if slug.include? 'pre-exists'
+        updated_slugs << "'#{slug}'"
         within('fieldset#short-urls-to-update') do
           expect(page).to have_content(slug)
           expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
         end
+      # ... and the ones that need to be created.
       else
-        # ... and the ones that need to be created.
+        created_slugs << "'#{slug}'"
         within('fieldset#short-urls-to-create') do
           expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
           expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
@@ -49,12 +54,11 @@ RSpec.feature "ShortUrls", type: :feature do
       end
     end
 
-    # Clear one of the inputs in each group to cause an error
-
-    # Expect that some will have succeeded, the flash shows them
-
-    # Expect that the ones we broke are still here to be created/updated and show relevant errors
-
-    # Fix them, and resubmit; expect that we are redirected to '#index' and the flash shows the remaining ones
+    click_button 'Submit short URLs for update and creation'
+    
+    expect(page).to have_content("Short URLs #{updated_slugs.join(', ')} were updated. Short URLs #{created_slugs.join(', ')} were created.")
   end
+
+  scenario 'user submits a batch of short URLs with bad URLs to create'
+  scenario 'user submits a batch of short URLs with bad URLs to update'
 end
