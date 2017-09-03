8c9c381b9fd1d2a2179891c9b88febd4392f84ef
SeanDMR <sean@flyinggrizzly.io>
2017-09-03 17:04:04 +0100

    finish refactor of batch feature spec

--- spec/features/short_urls_spec.rb
+++ spec/features/short_urls_spec.rb
@@ -37,7 +37,7 @@ RSpec.feature "ShortUrls", type: :feature do
     end
 
     scenario 'admin logs in and submits a valid batch', aggregate_failures: true, batch_test: true do
-      CSV_NAME = 'batch.csv'.freeze
+      csv_name = 'batch.csv'.freeze
 
       visit root_or_admin_path
       click_link   'Log in'
@@ -45,11 +45,11 @@ RSpec.feature "ShortUrls", type: :feature do
 
       click_link   'Create a Short URL'
       click_link   'Batch update and create'
-      attach_file  'short_urls_csv', "spec/support/files/#{CSV_NAME}"
+      attach_file  'short_urls_csv', "spec/support/files/#{csv_name}"
       click_button 'Create short URLs'
 
       # track slugs being updated and created to test success in a minute
-      updated_urls, created_urls = identify_urls_to_update_and_create(CSV_NAME)
+      updated_urls, created_urls = identify_urls_to_update_and_create(csv_name)
 
       # Expect each of the entries in the CSV to be present in the form
       updated_urls.each do |slug, redirect|
@@ -71,137 +71,133 @@ RSpec.feature "ShortUrls", type: :feature do
     end
 
     scenario 'admin submits a batch with bad URLs to update', :aggregate_failures do
+      csv_name = 'batch_with_bad_urls_to_update.csv'.freeze
+
       visit root_or_admin_path
       click_link   'Log in'
       fill_in_login_details_and_log_in_as(@admin)
 
       click_link   'Create a Short URL'
       click_link   'Batch update and create'
-      attach_file  'short_urls_csv', 'spec/support/files/batch_with_bad_urls_to_update.csv'
+      attach_file  'short_urls_csv', "spec/support/files/#{csv_name}"
       click_button 'Create short URLs'
 
       # track slugs being updated and created to test success in a minute
-      updated_slugs = []; created_slugs = []
+      updated_urls, created_urls = identify_urls_to_update_and_create(csv_name)
 
-      # Expect each of the entries in the CSV to be present in the form...
-      CSV.read('spec/support/files/batch_with_bad_urls_to_update.csv').each do |slug, redirect|
-        # ... those that need to be updated
-        if slug.include? 'pre-exists'
-          updated_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-update') do
-            expect(page).to have_content(slug)
-            expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
-          end
-        # ... and the ones that need to be created.
-        else
-          created_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-create') do
-            expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
-            expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
-          end
+      # Expect each of the entries in the CSV to be present in the form
+      updated_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-update') do
+          expect(page).to have_content(slug)
+          expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
+        end
+      end
+      created_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-create') do
+          expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
+          expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
         end
       end
 
       click_button 'Submit short URLs for update and creation'
 
-      CSV.read('spec/support/files/batch_with_bad_urls_to_update.csv').each do |slug, redirect|
-        next unless slug.include? 'pre-exist'
+      # The URLs to be updated should still be in the form
+      updated_urls.each do |slug, redirect|
         expect(page).to have_content(slug)
         expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
       end
 
-      expect(page).to have_content("Short URLs #{created_slugs.join(', ')} were created.")
+      # Make sure that the URLs to be created happened
+      expect(page).to have_content("Short URLs #{slugs_for_flash(created_urls)} were created.")
+
+      # Make sure we're being told there were issues
       expect(page).to have_content("Some of your short URLs had problems. Please fix these. This has not prevented the valid ones from being updated or created.")
       expect(page).to have_css('.error_explanation ul li.error-item', count: 2)
     end
 
     scenario 'admin submits a batch with bad URLs to create', :aggregate_failures do
+      csv_name = 'batch_with_bad_urls_to_create.csv'.freeze
+
       visit root_or_admin_path
       click_link   'Log in'
       fill_in_login_details_and_log_in_as(@admin)
 
       click_link   'Create a Short URL'
       click_link   'Batch update and create'
-      attach_file  'short_urls_csv', 'spec/support/files/batch_with_bad_urls_to_create.csv'
+      attach_file  'short_urls_csv', "spec/support/files/#{csv_name}"
       click_button 'Create short URLs'
 
       # track slugs being updated and created to test success in a minute
-      updated_slugs = []; created_slugs = []
+      updated_urls, created_urls = identify_urls_to_update_and_create(csv_name)
 
-      # Expect each of the entries in the CSV to be present in the form...
-      CSV.read('spec/support/files/batch_with_bad_urls_to_create.csv').each do |slug, redirect|
-        # ... those that need to be updated
-        if slug.include? 'pre-exists'
-          updated_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-update') do
-            expect(page).to have_content(slug)
-            expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
-          end
-        # ... and the ones that need to be created.
-        else
-          created_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-create') do
-            expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
-            expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
-          end
+      # Expect each of the entries in the CSV to be present in the form
+      updated_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-update') do
+          expect(page).to have_content(slug)
+          expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
+        end
+      end
+      created_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-create') do
+          expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
+          expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
         end
       end
 
       click_button 'Submit short URLs for update and creation'
 
-      CSV.read('spec/support/files/batch_with_bad_urls_to_create.csv').each do |slug, redirect|
-        next unless slug.include? 'new'
+      # The URLs to be created should still be in the form
+      created_urls.each do |slug, redirect|
         expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
         expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
       end
 
-      expect(page).to have_content("Short URLs #{updated_slugs.join(', ')} were updated.")
+      # We should have confirmation that the updates worked
+      expect(page).to have_content("Short URLs #{slugs_for_flash(updated_urls)} were updated.")
+
+      # We should also be told that there were problems, but they didn't interfere with the successful operations
       expect(page).to have_content("Some of your short URLs had problems. Please fix these. This has not prevented the valid ones from being updated or created.")
       expect(page).to have_css('.error_explanation ul li.error-item', count: 2)
     end
 
     scenario 'admin submits a batch with all bad URLs', :aggregate_failures do
+      csv_name = 'batch_all_bad.csv'.freeze
+
       visit root_or_admin_path
       click_link   'Log in'
       fill_in_login_details_and_log_in_as(@admin)
 
       click_link   'Create a Short URL'
       click_link   'Batch update and create'
-      attach_file  'short_urls_csv', 'spec/support/files/batch_all_bad.csv'
+      attach_file  'short_urls_csv', "spec/support/files/#{csv_name}"
       click_button 'Create short URLs'
 
       # track slugs being updated and created to test success in a minute
-      updated_slugs = []; created_slugs = []
+      updated_urls, created_urls = identify_urls_to_update_and_create(csv_name)
 
-      # Expect each of the entries in the CSV to be present in the form...
-      CSV.read('spec/support/files/batch_all_bad.csv').each do |slug, redirect|
-        # ... those that need to be updated
-        if slug.include? 'pre-exists'
-          updated_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-update') do
-            expect(page).to have_content(slug)
-            expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
-          end
-        # ... and the ones that need to be created.
-        else
-          created_slugs << "'#{slug}'"
-          within('fieldset#short-urls-to-create') do
-            expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
-            expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
-          end
+      # Expect each of the entries in the CSV to be present in the form
+      updated_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-update') do
+          expect(page).to have_content(slug)
+          expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
+        end
+      end
+      created_urls.each do |slug, redirect|
+        within('fieldset#short-urls-to-create') do
+          expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
+          expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
         end
       end
 
       click_button 'Submit short URLs for update and creation'
 
-      CSV.read('spec/support/files/batch_all_bad.csv').each do |slug, redirect|
-        if slug.include? 'new'
-          expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
-          expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
-        else
-          expect(page).to have_content(slug)
-          expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
-        end
+      updated_urls.each do |slug, redirect|
+        expect(page).to have_content(slug)
+        expect(page).to have_field("short_urls_to_update[#{slug}][redirect]", with: redirect)
+      end
+      created_urls.each do |slug, redirect|
+        expect(page).to have_field("short_urls_to_create[#{slug}][slug]", with: slug)
+        expect(page).to have_field("short_urls_to_create[#{slug}][redirect]", with: redirect)
       end
 
       expect(page).to have_content("Your short URLs had some problems. Please fix these.")
