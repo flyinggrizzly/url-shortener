95042fff66d2a037d18fa3694b2c81273d704229
SeanDMR <sean@flyinggrizzly.io>
2017-09-03 18:00:44 +0100

    wip batch controller concern spec

--- spec/controllers/concerns/batchable_controller_concern_spec.rb
+++ spec/controllers/concerns/batchable_controller_concern_spec.rb
@@ -1,52 +1,58 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Batchable, type: :controller do
+RSpec.describe Batchable, type: :controller do
 
-#   # Declare an anonymous controller to test the concern on
-#   controller(ApplicationController) do
-#     include Batchable
+  # Declare an anonymous controller to test the concern on
+  controller(ApplicationController) do
+    include Batchable
 
-#     # Expose private methods for unit tests
-#     public :identify_action_for_record,
-#            :read_batch_csv,
-#            :short_urls_csv_params,
-#            :short_urls_batch_params,
-#            :upload
-#   end
+    # Expose private methods for unit tests
+    public :identify_action_for_record,
+           :read_batch_csv,
+           :short_urls_csv_params,
+           :upload
+  end
 
-#   before { routes.draw { get 'batch' => 'anonymous#batch' } }
+  before do
+    routes.draw do
+      get  'batch' => 'anonymous#batch'
+      post 'batch' => 'anonymous#batch_edit_and_new'
+    end
+  end
 
 
-#   describe '#batch' do
-#     # before { get :fake_action }
-#     # it { expect(response).to redirect_to('/an_url')}
-#     it 'responds successfully' do
-#       get :batch
-#       expect(response).to be_success
-#     end
-#   end
+  describe '#batch' do
+    it 'responds successfully' do
+      get :batch
+      expect(response).to be_success
+    end
+  end
 
-#   describe '#batch_edit_and_new' do
-#   end
+  describe '#batch_edit_and_new' do
+    it 'responds successfully' do
+      post :batch_edit_and_and_new
+      expect(response).to be_success
+    end
+  end
 
-#   describe '#batch_update_and_create' do
-#   end
+  describe '#batch_update_and_create' do
+  end
 
-#   describe '#identify_action_for_record(records)' do
-#   end
+  describe '#identify_action_for_record(records)' do
+  end
 
-#   describe '#read_batch_csv' do
-#   end
+  describe '#read_batch_csv' do
+  end
 
-#   describe '#short_urls_csv_params' do
-#     pending
-#   end
+  describe '#short_urls_csv_params' do
+    pending
+  end
 
-#   describe '#short_urls_batch_params' do
-#     pending
-#   end
+  describe '#short_urls_batch_params' do
+    pending
+  end
 
-#   describe '#upload' do
+  describe '#upload' do
 
-#   end
-# end
+  end
+end
